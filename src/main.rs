38959f2d56ed5478d794712712747819631cc90f
ErisedKT <sanyaverma0303@gmail.com>
2020-10-24 14:55:02 -0400

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 extern crate merkletree;
-extern crate crypto;
+use sha1::{Sha1, Digest};
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+
+}
+
+fn get_chunks(filename: &str) -> Vec<Vec<u8>> {
+    let mut f = File::open(filename).expect("File not found!");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    let mut chunks: 
 }
